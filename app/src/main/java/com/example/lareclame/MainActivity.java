501bae51667b03689478ed037313f1cdebae39d4
Alpaca <ayazhan1121@gmail.com>
2022-04-16 12:31:58 +0600

    init commit

---
+++ app/src/main/java/com/example/lareclame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lareclame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View.OnClickListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/lareclame/RegisterActivity.java
@@ -0,0 +1,56 @@
+package com.example.lareclame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText username;
+    EditText password;
+    Button sign_up;
+    TextView sign_in;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        email = findViewById(R.id.sign_up_email_address);
+        username = findViewById(R.id.sign_up_username);
+        password = findViewById(R.id.sign_up_password);
+        sign_up = findViewById(R.id.sign_up_button);
+
+        sign_up.setOnClickListener(view -> checkIfDataEntered());
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence c = text.getText().toString();
+        return TextUtils.isEmpty(c);
+    }
+
+    private void checkIfDataEntered() {
+        if (isEmpty(username)) {
+            Toast toast=Toast.makeText(this, "Username is required!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        if (!isEmail(email)) {
+            email.setError("Enter valid email");
+        }
+    }
+}
