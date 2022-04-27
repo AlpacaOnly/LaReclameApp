a1a31fc26f9adcf0af89186dbbd60af8f27a36b7
mercurial <dias.nespayev@gmail.com>
2022-04-28 03:34:33 +0600

    chore: add bottom navigation listener

--- app/src/main/java/com/example/lareclame/auth/CreateItemActivity.java
+++ app/src/main/java/com/example/lareclame/auth/CreateItemActivity.java
@@ -1,14 +1,18 @@
 package com.example.lareclame.auth;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.lareclame.MainActivity;
 import com.example.lareclame.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CreateItemActivity extends AppCompatActivity {
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,19 @@ public class CreateItemActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.ic_create_announcement);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.ic_home:
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                case R.id.ic_create_announcement:
+                    startActivity(new Intent(getApplicationContext(), CreateItemActivity.class));
+                case R.id.ic_settings:
+                    return true;
+                case R.id.ic_profile:
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+            }
+            return false;
+        });
     }
 }
