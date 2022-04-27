d71d8046a9d287cc65934ee6d178c99bde1a3a42
mercurial <dias.nespayev@gmail.com>
2022-04-28 03:33:26 +0600

    chore: select create_announcement icon

--- app/src/main/java/com/example/lareclame/auth/CreateItemActivity.java
+++ app/src/main/java/com/example/lareclame/auth/CreateItemActivity.java
@@ -6,11 +6,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lareclame.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CreateItemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.ic_create_announcement);
     }
 }
