9b2e810829d0d6bf45515bf354d0d4fedb2f5b44
Alpaca <ayazhan1121@gmail.com>
2022-04-29 17:01:37 +0600

    chore: add methods in recyclerAdapter

--- app/src/main/java/com/example/lareclame/recyclerAdapter.java
+++ app/src/main/java/com/example/lareclame/recyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.lareclame;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,34 +11,37 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder> {
-    private ArrayList <Item> itemList;
+    private ArrayList <Item> itemsList;
 
-    public recyclerAdapter(ArrayList <Item> itemList) {
-        this.itemList=itemList;
+    public recyclerAdapter(ArrayList <Item> itemsList) {
+        this.itemsList =itemsList;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView tv_name;
-        public MyViewHolder(final View itemView) {
-            super(itemView);
-            tv_name=itemView.findViewById(R.id.item_name);
+
+        public MyViewHolder(final View View) {
+            super(View);
+            tv_name=View.findViewById(R.id.item_name);
         }
     }
 
     @NonNull
     @Override
     public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull recyclerAdapter.MyViewHolder holder, int position) {
-
+        String name= itemsList.get(position).getName();
+        holder.tv_name.setText(name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemsList.size();
     }
 
 }
