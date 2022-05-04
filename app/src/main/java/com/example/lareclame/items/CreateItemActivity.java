7b04ffcda965412c61e9f24b9e12382e2b04af09
Alpaca <ayazhan1121@gmail.com>
2022-05-04 22:55:15 +0600

    feat: add create_item onclick create

--- app/src/main/java/com/example/lareclame/items/CreateItemActivity.java
+++ app/src/main/java/com/example/lareclame/items/CreateItemActivity.java
@@ -1,27 +1,47 @@
 package com.example.lareclame.items;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 import com.example.lareclame.MainActivity;
 import com.example.lareclame.ProfileActivity;
 import com.example.lareclame.R;
 import com.example.lareclame.SettingsActivity;
+import com.example.lareclame.auth.LoginActivity;
+import com.example.lareclame.auth.RegisterActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CreateItemActivity extends AppCompatActivity {
+    EditText et_username;
+    EditText et_title;
+    EditText et_body;
+
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+        et_username = findViewById(R.id.username);
+        et_title = findViewById(R.id.title);
+        et_body = findViewById(R.id.body);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.ic_create_announcement);
@@ -43,4 +63,41 @@ public class CreateItemActivity extends AppCompatActivity {
             return false;
         });
     }
+
+    public void create_item() {
+        final String username = et_username.getText().toString();
+        final String title = et_title.getText().toString();
+        final String body = et_body.getText().toString();
+
+        Response.Listener <String> listener = response -> {
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                String status =jsonObject.getString("status");
+
+                if (status.equals("ok")) {
+                    Toast toast=Toast.makeText(this, "Successfully added!", Toast.LENGTH_SHORT);
+                    toast.show();
+                    Intent intent = new Intent(this, CreateItemActivity.class);
+                    startActivity(intent);
+                } else {
+                    String error = jsonObject.getString("error");
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateItemActivity.this);
+                    builder.setMessage(error).setNegativeButton("Retry", null).create().show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        };
+
+        ItemRequest itemRequest = new ItemRequest(username, title, body, listener, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        });
+
+        RequestQueue itemQueue = Volley.newRequestQueue(this);
+        itemQueue.add(itemRequest);
+    }
+
 }
