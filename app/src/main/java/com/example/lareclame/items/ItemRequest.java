915ea27afcc97533c2c17b839cf4df02d0dce4f6
Alpaca <ayazhan1121@gmail.com>
2022-05-04 21:25:48 +0600

    feat: add ItemRequest class

---
+++ app/src/main/java/com/example/lareclame/items/ItemRequest.java
@@ -0,0 +1,21 @@
+package com.example.lareclame.items;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemRequest extends StringRequest {
+    private static final String LoginRequestUrl="http://134.209.109.73:5001/items/add/item";
+    private final Map<String, String> params;
+
+    public ItemRequest(String username, String title, String body, Response.Listener<String> listener, Response.ErrorListener err) {
+        super(Method.POST, LoginRequestUrl, listener, err);
+        params=new HashMap<>();
+        params.put("username", username);
+        params.put("title", title);
+        params.put("body", body);
+
+    }
+}
