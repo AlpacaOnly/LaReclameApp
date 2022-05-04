1c2c880f39d5014e8cab5b6385874d9bafb1986c
Alpaca <ayazhan1121@gmail.com>
2022-05-04 15:36:56 +0600

    feat: change profile photo

--- app/src/main/java/com/example/lareclame/ProfileActivity.java
+++ app/src/main/java/com/example/lareclame/ProfileActivity.java
@@ -3,7 +3,13 @@ package com.example.lareclame;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,7 +17,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView ProfileImage;
+    private static final int PICK_IMAGE = 1;
+    Uri ImageUrl;
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -24,6 +38,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         final TextView tv_username = (TextView) findViewById(R.id.username);
         tv_username.setText(nm);
+
+        ProfileImage = (ImageView) findViewById(R.id.user_photo);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.ic_profile);
@@ -47,4 +63,46 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
 
+    public void change_photo(View view) {
+        Intent gallery = new Intent();
+        gallery.setType("image/*");
+        gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(gallery,"Select Picture"), PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            ImageUrl = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), ImageUrl);
+                SaveImage(bitmap);
+                ProfileImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void SaveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+
+        File file = new File (myDir, System.currentTimeMillis()+".jpg");
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
