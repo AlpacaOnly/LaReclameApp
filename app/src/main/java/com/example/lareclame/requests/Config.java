7a198f99d06984a7da77d89775da915d13e7920a
mercurial <dias.nespayev@gmail.com>
2022-05-13 11:33:03 +0600

    chore: change host

--- app/src/main/java/com/example/lareclame/requests/Config.java
+++ app/src/main/java/com/example/lareclame/requests/Config.java
@@ -1,6 +1,6 @@
 package com.example.lareclame.requests;
 
 public class Config {
-    static String host = "10.0.2.2";
-    static String port = "5000";
+    static String host = "134.209.109.73";
+    static String port = "5001";
 }
