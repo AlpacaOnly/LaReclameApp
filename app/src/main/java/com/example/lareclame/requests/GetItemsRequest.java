8698900a21dc6666136ca93f6534b63d24196bef
mercurial <dias.nespayev@gmail.com>
2022-05-24 19:03:46 +0600

    feat: show items in profile page
    scroll not working

--- app/src/main/java/com/example/lareclame/requests/GetItemsRequest.java
+++ app/src/main/java/com/example/lareclame/requests/GetItemsRequest.java
@@ -5,17 +5,19 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
 public class GetItemsRequest extends StringRequest{
     private static final String LoginRequestUrl="http://" + Config.host + ":" + Config.port + "/api/items";
     private final Map <String, String> params;
 
-    public GetItemsRequest(Response.Listener<String> listener, Response.ErrorListener err, String search, int category_id) {
+    public GetItemsRequest(Response.Listener<String> listener, Response.ErrorListener err, String search, int category_id, int user_id) {
         super(Method.POST, LoginRequestUrl, listener, err);
         params = new HashMap<>();
-        params.put("search", search);
+        if (!Objects.equals(search, "")) params.put("search", search);
         if (category_id != 0) params.put("filter_by", category_id + "");
+        if (user_id != 0) params.put("user_id", user_id + "");
     }
 
     @Override
