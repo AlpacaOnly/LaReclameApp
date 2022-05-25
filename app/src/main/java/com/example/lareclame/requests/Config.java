f884d76b12fc39199f0242f4b8a0fbf93bb37bd1
mercurial <dias.nespayev@gmail.com>
2022-05-25 07:49:56 +0600

    fix: Config

--- app/src/main/java/com/example/lareclame/requests/Config.java
+++ app/src/main/java/com/example/lareclame/requests/Config.java
@@ -1,7 +1,6 @@
 package com.example.lareclame.requests;
 
 public class Config {
-//    static String host = "134.209.109.73";
-    static String host = "10.0.2.2";
+    static String host = "134.209.109.73";
     static String port = "5000";
 }
